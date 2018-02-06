7d16dfcf7cb8ca7a8719a045edd8e8fb41639a34
infinite-Zh <zhxuwork@163.com>
2018-02-07 00:51:51 +0800

    add edit-task fragment

---
+++ app/src/main/java/com/infinite/todoapp/editTask/EditTaskFragment.kt
@@ -0,0 +1,16 @@
+package com.infinite.todoapp.editTask
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by xz on 2018/2/7.
+ */
+class EditTaskFragment:Fragment(){
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
