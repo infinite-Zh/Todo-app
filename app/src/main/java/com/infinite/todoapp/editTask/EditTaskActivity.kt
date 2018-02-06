ee3c76e63b198b6d684c82d55003d34c161d390f
infinite-Zh <zhxuwork@163.com>
2018-02-07 00:29:12 +0800

    add edit-task activity

---
+++ app/src/main/java/com/infinite/todoapp/editTask/EditTaskActivity.kt
@@ -0,0 +1,13 @@
+package com.infinite.todoapp.editTask
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by xz on 2018/2/7.
+ */
+class EditTaskActivity:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
