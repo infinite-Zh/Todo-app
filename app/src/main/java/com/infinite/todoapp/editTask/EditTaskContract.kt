3135fe353fcdce70dcc1c1e7f4ed8a48dcb47885
infinite-Zh <zhxuwork@163.com>
2018-02-08 22:36:46 +0800

    edit edittaskcontract

--- app/src/main/java/com/infinite/todoapp/editTask/EditTaskContract.kt
+++ app/src/main/java/com/infinite/todoapp/editTask/EditTaskContract.kt
@@ -1,8 +1,17 @@
 package com.infinite.todoapp.editTask
 
+import com.infinite.todoapp.IBasePresenter
+import com.infinite.todoapp.IBaseView
+
 /**
  * Created by xz on 2018/2/7.
  */
 class EditTaskContract{
 
+     interface Presenter :IBasePresenter{
+         fun updateTask()
+     }
+    interface View :IBaseView<Presenter>{
+        fun updateTask(title:String,description:String)
+    }
 }
