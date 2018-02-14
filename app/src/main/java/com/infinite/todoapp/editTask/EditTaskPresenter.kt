ef239b4b6426a9a03999474b4ada616130186e93
infinite-Zh <zhxuwork@163.com>
2018-02-14 21:00:10 +0800

    add edittaskpresenter

---
+++ app/src/main/java/com/infinite/todoapp/editTask/EditTaskPresenter.kt
@@ -0,0 +1,18 @@
+package com.infinite.todoapp.editTask
+
+/**
+ * Created by xz on 2018/2/14.
+ */
+class EditTaskPresenter:EditTaskContract.Presenter{
+    override fun start() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun updateTask(title: String, description: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun loadTask() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
